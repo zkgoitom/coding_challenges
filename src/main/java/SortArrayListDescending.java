68e6cf4d482b8bffc0402df26fc843008a7eecd6
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-17 00:48:17 -0400

    practicing sorting an ArrayList in descending order using reverseOrder()

--- src/main/java/SortArrayListDescending.java
+++ src/main/java/SortArrayListDescending.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SortArrayListDescending {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortArrayListDescending {
+    public static void main (String [] args) {
+
+        ArrayList<String> arrList = new ArrayList<>();
+
+        arrList.add("Bron");
+        arrList.add("Kobe");
+        arrList.add("Mike");
+
+        // Collections.sort() sorts objects in ascending order, unless you pass the Collections.reverseOrder() method as the 2nd argument
+        // to the Collections.sort(). invoking reverseOrder() inside sort method
+        Collections.sort(arrList, Collections.reverseOrder());
+
+        System.out.println("List of objects are in descending order: \n" + arrList);
+    }
 }
